4b14a2d7e922a2bfb7ef74f4ef2f0e365a78e161
Bill W <42258241+wwunsch23@users.noreply.github.com>
2018-12-02 21:23:55 -0600

    Created storing survey data functions

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends.
+// ===============================================================================
+
+var friendArray = [];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendArray;
+  
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -10,3 +9,14 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
